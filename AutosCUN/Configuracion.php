4fc233d88ab6804ef8bbaa650a53aab95b5fc11b
cpaez-art <61921778+cpaez-art@users.noreply.github.com>
2020-03-07 18:43:20 -0500

    Update Configuracion.php

--- AutosCUN/Configuracion.php
+++ AutosCUN/Configuracion.php
@@ -1,14 +1,16 @@
 <?php
-//DB details
-$dbHost = 'proeycto2020.database.windows.net';
-$dbUsername = 'cpaez';
-$dbPassword = 'Cristian2020*';
-$dbName = 'autoscun';
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:proeycto2020.database.windows.net,1433; Database = autoscun", "cpaez", "Cristian2020*");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-//Create connection and select DB
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-if ($db->connect_error) {
-    die("No hay Conexion con la base de datos: " . $db->connect_error);
-} 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "cpaez", "pwd" => "Cristian2020*", "Database" => "autoscun", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:proeycto2020.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
